99c122abd4ee9a16fd1bb60ec8a2226775ec5160
Khairil Yusof <khairil.yusof@gmail.com>
2022-09-22 10:08:20 +0800

    Update with more granularity for orang asal

--- src/sinar/miscbehavior/vocabularies/marginalized_communities_malaysia.py
+++ src/sinar/miscbehavior/vocabularies/marginalized_communities_malaysia.py
@@ -24,8 +24,15 @@ class MarginalizedCommunitiesMalaysia(object):
         # Just an example list of content for our vocabulary,
         # this can be any static or dynamic data, a catalog result for example.
         items = [
-            VocabItem(u'sony-a7r-iii', _(u'Sony Aplha 7R III')),
-            VocabItem(u'canon-5d-iv', _(u'Canon 5D IV')),
+            VocabItem(u'orang-asal-sabah', _(u'Orang Asal in Sabah')),
+            VocabItem(u'orang-asal-sarawak', _(u'Orang Asal in Sarawak')),
+            VocabItem(u'sex-workers', _(u'Sex Workers')),
+            VocabItem(u'refugees', _(u'Refugees')),
+            VocabItem(u'contract-labour', _(u'Contract Labour')),
+            VocabItem(u'domestic-violence', _(u'Victims of Domestic Violence')),
+            VocabItem(u'disabled', _(u'Persons with Disabilities')),
+            VocabItem(u'rural-communities', _(u'Rural Communities')),
+            VocabItem(u'stateless-persons', _(u'Stateles Persons')),
         ]
 
         # Fix context if you are using the vocabulary in DataGridField.

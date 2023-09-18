732eb83f3ddbae90b43a323af071e3c8b4d7ada9
Khairil Yusof <khairil.yusof@gmail.com>
2023-09-19 04:25:19 +0800

    Add Assessment behavior field
    This field is a rich text field to add general notes, needed
    for narrative assessment of a project activity

--- src/sinar/miscbehavior/behaviors/assessment.py
+++ src/sinar/miscbehavior/behaviors/assessment.py
@@ -2,8 +2,11 @@
 
 from sinar.miscbehavior import _
 from plone import schema
+from collective import dexteritytextindexer
+from plone.app.textfield import RichText
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel import model
+from plone.autoform import directives
 from Products.CMFPlone.utils import safe_hasattr
 from zope.component import adapter
 from zope.interface import Interface
@@ -20,12 +23,32 @@ class IAssessment(model.Schema):
     """
     """
 
-    project = schema.TextLine(
-        title=_(u'Project'),
-        description=_(u'Give in a project name'),
+    dexteritytextindexer.searchable('assessment_notes')
+    directives.read_permission(assessment_notes='cmf.ModifyPortalContent')
+    directives.write_permission(assessment_notes='cmf.ModifyPortalContent')
+    assessment_notes = RichText(
+        title=_('Assessment Notes'),
+        description=_('''
+                             '''),
+        default=_('''
+                 <p class="callout">
+                 Describe any partnerships with other organizations,
+                 researchers and community leaders that have been
+                 developed during the project cycle and the usefulness
+                 of these in achieving the project's objectives.
+                 <br />
+                 Describe the involvement of project beneficiaries,
+                 during all phases of project implementation.
+                 <br />
+                 Describe any gender, ethnic and generation gap issues that have impacted positively or negatively your project implementation.
+                 <br />
+                 Please take the time to reflect about activities that you struggle to implement during the period reported, along with processes and methods originally planned that might need adjustment to achieve your project objectives.
+                 </p>
+                 <p>
+                 </p>
+                 '''),
         required=False,
     )
-
 
 @implementer(IAssessment)
 @adapter(IAssessmentMarker)
@@ -34,11 +57,11 @@ class Assessment(object):
         self.context = context
 
     @property
-    def project(self):
-        if safe_hasattr(self.context, 'project'):
-            return self.context.project
+    def assessment_notes(self):
+        if safe_hasattr(self.context, 'assessment_notes'):
+            return self.context.assessment_notes
         return None
 
-    @project.setter
-    def project(self, value):
-        self.context.project = value
+    @assessment_notes.setter
+    def assessment_notes(self, value):
+        self.context.assessment_notes = value
